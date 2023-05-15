3051f47dca90e39be4db5747c2b30ecbfa2e0838
Reynan Vieira <102336632+ReynanVv@users.noreply.github.com>
2023-05-14 21:40:05 -0300

    Media Geometrica

---
+++ MediaGeometrica.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MediaGeometrica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Double> valores = new ArrayList();
+        while (valores.size() < 2) {
+            System.out.println("Digite um valor qualquer: ");
+            Double valor = sc.nextDouble();
+            valores.add(valor);
+        }
+        double multi = 1;
+        for (Double valor : valores){
+            multi *= valor;
+        }
+        float media = (float) Math.sqrt(multi);
+        System.out.println(String.format("A média geométrica dos números digitados é: %.2f", media));
+        sc.close();
+    }
+    
+}
