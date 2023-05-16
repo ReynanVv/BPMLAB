72dfbdecd86612dfb1726c13e4fdabc5bbeb019b
Micro <reynanvt@gmail.com>
2023-05-16 01:53:46 -0300

    Lei de Ohm

---
+++ LeiDeOhm.java
@@ -0,0 +1,15 @@
+// U = R * I
+import java.util.Scanner;
+
+public class LeiDeOhm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor da resistência elétrica: ");
+        double R = sc.nextDouble();
+        System.out.println("Digite o valor da corrente elétrica: ");
+        double I = sc.nextDouble();
+        double U = R * I;
+        System.out.println(String.format("Tensão do circuito eletrônico: %.2f", U));
+        sc.close();
+    }
+}
